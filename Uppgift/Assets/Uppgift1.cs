f126cc70b6764426d17ba5eb6e187f2702418310
Daniel Ekström <daniel.ekstrm08@gmil.com>
2017-01-23 11:37:04 +0100

    Påbörjad uppgift

---
+++ Uppgift/Assets/Uppgift1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift1 : MonoBehaviour {
+    public int a = 10;
+    public int b = 20;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(a);
+        Debug.Log(b);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
