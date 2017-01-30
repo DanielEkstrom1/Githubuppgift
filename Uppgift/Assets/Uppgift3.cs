0cea6585389589e6dafd92206232d104f512527d
Daniel Ekström <daniel.ekstrom1@learnet.se>
2017-01-30 13:15:57 +0100

    Klar med uppgift 3

---
+++ Uppgift/Assets/Uppgift3.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift3 : MonoBehaviour {
+    public int a;
+    public int b;
+
+    int Add(int a, int b) {
+        return a + b;
+    }
+    // Use this for initialization
+    void Start() {
+
+        int t = Add(a, b);
+        Debug.Log(t);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Uppgift/Assets/Uppgift4.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift4 : MonoBehaviour {
+    public float valA = 5;
+    public float valC = 10;
+    public float valB;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (valB < valA)
+
+        {
+            Debug.Log("B is smaller");
+        }
+
+        else if (valB > valC)
+
+        {
+            Debug.Log("B is Bigger");
+        }
+
+        else if (valB > valA)
+
+        {
+            Debug.Log("B is in between");
+        }
+
+        else
+
+        {
+            Debug.Log("D is in between ");
+        }
+    }
+}
+
