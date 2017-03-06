02474404aa7e9613efb0c40cfcf8d26a4733c236
Daniel Ekström <daniel.ekstrm08@gmil.com>
2017-03-06 13:04:15 +0100

    klar med uppgift 5

--- Uppgift/Assets/Uppgift5.cs
+++ Uppgift/Assets/Uppgift5.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Uppgift5 : MonoBehaviour {
+public class Uppgift5 : MonoBehaviour
+{
+    public int valA = 20;
+    public int valB = 10;
+    public int valC;
+    public float valD = 20;
+    public float valE = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    int Method(int a, int b)
+    {
+        return a + b;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        valC = Method(valA, valB);
+        if (valC < valE || valC > valD)
+        {
+            Debug.Log("större eller mindre än D och E");
+        }
+        else
+        {
+            Debug.Log("Mellan D och E");
+        }
+    }
 }
